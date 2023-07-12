1a6b1c733a4ad4dd4cb3b17edf66cd477987cc90
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-12 13:09:40 -0300

    removendo decorator @Repository ~n necessario

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,9 +1,7 @@
 package br.com.banco.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.banco.entities.TransferenciaEntity;
 
-@Repository
 public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, Long> { }
