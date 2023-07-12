02a2f805992c88cf75309671b3ecf1c21b7aad20
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-12 14:25:53 -0300

    adicionando mais uma propriedade para contaDTO

--- src/main/java/br/com/banco/dtos/ContaDto.java
+++ src/main/java/br/com/banco/dtos/ContaDto.java
@@ -1,7 +1,7 @@
 package br.com.banco.dtos;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
 
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -10,10 +10,12 @@ import lombok.Data;
 @Data
 public class ContaDto {
     @NotBlank
-    @Size(max = 11)
     @CPF
     private String cpf;
 
     @NotBlank
     private String senha;
+
+    @NotBlank
+    private String nomeResponsavel;
 }
