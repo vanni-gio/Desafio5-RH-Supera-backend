69d3f108e15572e31aef7306a6cc39a89b22a9c9
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-13 19:30:55 -0300

    implementando rota GET transferencias

--- src/main/java/br/com/banco/controllers/BancoController.java
+++ src/main/java/br/com/banco/controllers/BancoController.java
@@ -1,5 +1,8 @@
 package br.com.banco.controllers;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.banco.entities.TransferenciaEntity;
 import br.com.banco.services.implementation.ContaServiceImpl;
 import br.com.banco.services.implementation.TransferenciaServiceImpl;
 
@@ -18,6 +24,8 @@ public class BancoController {
 
     @Autowired
     private ContaServiceImpl _contaService;
+
+    @Autowired
     private TransferenciaServiceImpl _transferenciaService;
 
 
@@ -32,13 +40,33 @@ public class BancoController {
     }
 
     @GetMapping("/transferencias")
-    public ResponseEntity<Object> getAllTransferencias()
+    public ResponseEntity<Object> getAllTransferencias(
+        @RequestParam(required = false) Date dataInicial,
+        @RequestParam(required = false) Date dataFinal,
+        @RequestParam(required = false) String nomeOperador
+    )
     {
-        var transferencias = _transferenciaService.getAll();
-        return ResponseEntity.status(HttpStatus.CREATED).body(transferencias);
+        List<TransferenciaEntity> transferencias = null;
+        if(dataInicial != null && dataFinal != null){
+            if(nomeOperador != null){
+                transferencias = _transferenciaService.getAllByNomeOperadorTransferenciaAndDataTransferenciaBetween(
+                    nomeOperador, dataInicial, dataFinal
+                    );
+            }else{
+                transferencias = _transferenciaService.getAllByDataTransferenciaBetween(dataInicial, dataFinal);
+            }
+        }else{
+            if(nomeOperador != null){
+                transferencias = _transferenciaService.getAllByNomeOperadorTransferencia(nomeOperador);
+            }else if(!(dataInicial == null && dataFinal == null)){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Os campos data inicial e final não devem ser nulos");
+            }else{
+                transferencias = _transferenciaService.getAll();
+            }
+        }
+
+       return ResponseEntity.status(HttpStatus.OK).body(transferencias);
     }
-
-
 
 
 }
