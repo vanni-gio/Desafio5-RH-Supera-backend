74b107cfb9883678de2e448f95a0fdebf5f4bd4c
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-13 16:02:06 -0300

    add novos métodos de filtro

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,7 +1,12 @@
 package br.com.banco.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.banco.entities.TransferenciaEntity;
 
-public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, Long> { }
+public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, Long> {
+    public List<TransferenciaEntity> findAllByNomeOperadorTransferencia();
+    public List<TransferenciaEntity> findAllByDataTransferenciaBetween();
+ }
