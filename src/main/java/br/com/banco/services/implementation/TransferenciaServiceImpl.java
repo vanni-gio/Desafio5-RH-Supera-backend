958cd299dc5491ad72538b80e3238bec41ffb71f
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 13:22:20 -0300

    criando função de filtrarTrasfrenciasConta

--- src/main/java/br/com/banco/services/implementation/TransferenciaServiceImpl.java
+++ src/main/java/br/com/banco/services/implementation/TransferenciaServiceImpl.java
@@ -1,15 +1,19 @@
 package br.com.banco.services.implementation;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.banco.entities.ContaEntity;
 import br.com.banco.entities.TransferenciaEntity;
 import br.com.banco.repositories.TransferenciaRepository;
 import br.com.banco.services.interfaces.TransferenciaServiceInterface;
@@ -52,4 +56,37 @@ public class TransferenciaServiceImpl implements TransferenciaServiceInterface{
             dataTransferenciaEnd);
     }
 
+    
+    public List<TransferenciaEntity> filterTransferenciasConta(
+        ContaEntity conta,
+        Date dataTransferenciaStart,
+        Date dataTransferenciaEnd,
+        String nomeOperador
+    ){
+        var transferencias = conta.getTransferencias();
+        List<TransferenciaEntity> filteredList = new ArrayList<>();
+        filteredList = transferencias.stream()
+            .filter(transferencia -> filterDataAndNomeOperador(transferencia, nomeOperador, dataTransferenciaStart, dataTransferenciaEnd))
+            .collect(Collectors.toList());
+
+        return filteredList;
+    }
+
+    public static boolean filterDataAndNomeOperador(TransferenciaEntity transferencia, String nomeOperador, Date start, Date end){
+        var nomeOperadorTransferencia = transferencia.getNomeOperadorTransferencia();
+        if(start != null && end != null){
+            // verifica se uma data está entre duas outras datas
+            Predicate<Date> datePredicated = (t) -> start.compareTo(t) * t.compareTo(end) >= 0;
+            if(nomeOperador == null)
+                return datePredicated.test(transferencia.getDataTransferencia());
+            else{
+                if(nomeOperadorTransferencia != null)
+                    return datePredicated.test(transferencia.getDataTransferencia()) && nomeOperadorTransferencia.equals(nomeOperador);
+            }
+        }else if(nomeOperador != null){
+            if(nomeOperadorTransferencia != null)
+                return nomeOperadorTransferencia.equals(nomeOperador);
+        }
+        return false;
+    }
 }
