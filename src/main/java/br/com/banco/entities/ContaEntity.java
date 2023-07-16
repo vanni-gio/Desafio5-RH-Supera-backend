5932d364707c01e1e3a2e69a2c79c19e0b9bc284
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 17:55:28 -0300

    add @NonNull e @RequiredArgsContructs

--- src/main/java/br/com/banco/entities/ContaEntity.java
+++ src/main/java/br/com/banco/entities/ContaEntity.java
@@ -16,28 +16,36 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Data
 @Table(name = "conta")
+@RequiredArgsConstructor
 public class ContaEntity {
    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_conta")
+    @NonNull
     private Long id;
     
     @Column(nullable = false, name = "saldo_total")
+    @NonNull
     private Double saldoTotal;
 
     @Column(nullable = false, name = "nro_conta")
+    @NonNull
     private Long nroConta;
 
     @Column(nullable = false, length = 11, name = "cpf")
+    @NonNull
     private String cpf;
 
     @Column(length = 50, name = "nome_responsavel")
+    @NonNull
     private String nomeResponsavel;
 
     @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "conta")
--- src/main/java/br/com/banco/entities/TransferenciaEntity.java
+++ src/main/java/br/com/banco/entities/TransferenciaEntity.java
@@ -17,10 +17,11 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.banco.types.TipoTransacao;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 
 
@@ -28,29 +29,35 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "transferencia")
+@RequiredArgsConstructor
 public class TransferenciaEntity {
 
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private Long id;
 
     @Column(nullable = false, name = "data_transferencia")
     @Temporal(TemporalType.TIMESTAMP)
+    @NonNull
     private Date dataTransferencia;
     
     @Column(nullable = false, name="valor")
-    private double valor;
+    @NonNull
+    private Double valor;
 
     @Column(nullable = false, name="tipo")
     @Enumerated(EnumType.STRING)
+    @NonNull
     private TipoTransacao tipo;
     
     @Column(nullable = false, name="nome_operador_transacao")
+    @NonNull
     private String nomeOperadorTransferencia;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "conta_id")
-    // @JsonIgnore
     private ContaEntity conta;
 
     //standard constructors, getters, setters
