887305a84d0d6db8e595c7229a363968e54d9536
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 15:20:22 -0300

    add método sumSaldoTotal com decorator @Query

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.banco.entities.TransferenciaEntity;
 
@@ -16,4 +17,6 @@ public interface TransferenciaRepository extends JpaRepository<TransferenciaEnti
         Date dataTransferenciaStart,
         Date dataTransferenciaEnd
     );
+    @Query("SELECT SUM(t.valor) FROM TransferenciaEntity t")
+    public double sumSaldoTotal();
  }
