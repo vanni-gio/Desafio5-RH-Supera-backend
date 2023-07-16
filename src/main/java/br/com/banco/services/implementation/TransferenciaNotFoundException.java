a3c46b6037c8e08d5a9091f9b76498ca8396b077
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 17:52:49 -0300

    definindo Exception para ContaServiceImpl

---
+++ src/main/java/br/com/banco/services/implementation/TransferenciaNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.banco.services.implementation;
+
+public class TransferenciaNotFoundException extends Exception {
+
+    public TransferenciaNotFoundException(String message){
+        super(message);
+    }
+}
