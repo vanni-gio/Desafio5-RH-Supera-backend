b21bcac089152cc8b0cf636a1344196a1ac27206
vanni-gio <giovannip.gomesss@gmail.com>
2023-07-16 15:21:03 -0300

    refac método getAllTransferencias

--- src/main/java/br/com/banco/controllers/BancoController.java
+++ src/main/java/br/com/banco/controllers/BancoController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.banco.dtos.TransferenciasDto;
 import br.com.banco.entities.ContaEntity;
-import br.com.banco.entities.TransferenciaEntity;
 import br.com.banco.services.implementation.ContaServiceImpl;
 import br.com.banco.services.implementation.TransferenciaServiceImpl;
 
@@ -35,23 +35,23 @@ public class BancoController {
         @RequestParam(required = false) String nomeOperador
     )
     {
-        List<TransferenciaEntity> transferencias = null;
-        if(!nullOrEmpty(dataInicial) && !nullOrEmpty(dataFinal)){
-            if(!nullOrEmpty(nomeOperador)){
-                transferencias = _transferenciaService.getAllByNomeOperadorTransferenciaAndDataTransferenciaBetween(
-                    nomeOperador, dataInicial, dataFinal
-                    );
-            }else{
-                transferencias = _transferenciaService.getAllByDataTransferenciaBetween(dataInicial, dataFinal);
-            }
-        }else{
-            if(!nullOrEmpty(nomeOperador)){
+        TransferenciasDto transferencias = _transferenciaService.getAll();
+        boolean isDataInicialNull = nullOrEmpty(dataInicial);
+        boolean isDataFinalNull = nullOrEmpty(dataFinal);
+        boolean isNomeOperadorEmptyOrNull = nullOrEmpty(nomeOperador);
+
+        if(isDataFinalNull && isDataInicialNull){
+            if(!isNomeOperadorEmptyOrNull)
                 transferencias = _transferenciaService.getAllByNomeOperadorTransferencia(nomeOperador);
-            }else if(!(nullOrEmpty(dataInicial) && nullOrEmpty(dataFinal))){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Os campos data inicial e final não devem ser nulos");
-            }else{
-                transferencias = _transferenciaService.getAll();
-            }
+            
+        }else if(!isDataFinalNull && !isDataInicialNull){
+                if(!isNomeOperadorEmptyOrNull)
+                    transferencias = _transferenciaService.getAllByNomeOperadorTransferenciaAndDataTransferenciaBetween(
+                    nomeOperador, dataInicial, dataFinal);
+                else
+                    transferencias = _transferenciaService.getAllByDataTransferenciaBetween(dataInicial, dataFinal);     
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Os campos data inicial e final não devem ser nulos");
         }
 
        return ResponseEntity.status(HttpStatus.OK).body(transferencias);
